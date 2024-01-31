4edc12066da364ba3ea0c0950a1c37aa2f713fdd
rood0039 <rood0039@umn.edu>
2024-01-31 17:50:18 -0600

    Binary Compressor implimented in full
    Still lacking documentation and demo HTML

--- Steganography/Code/src/compression.js
+++ Steganography/Code/src/compression.js
@@ -1,32 +1,86 @@
+import "./pako.js"
 //This Class acts as a wrapper for Pako.js, making sure text can be SAFELY conveted between raw and compressed Base64
-export class URICompression
+export class URICompressor
 {
     constructor()
     {
         //you shouldnt be instantiating this
     }
 
-    /**
-     * The function compresses the given string to base64 representation via Pako.js.
-     * @param str - The parameter "str" is a (UTF-32) string that represents the input that needs to be
-     * compressed.
-     * @return - Returns a compressed version of the data in base64
-     * @note - Function takes care to properly encode the text to handle the U8Int limitations of pajo.js.
-     */
-    static compress(str)
+    static compress(uncompressedBinary)
     {
+        if(!URICompressor.isBinaryString(uncompressedBinary))
+        {
+            console.warn("could not compress string, not binary");
+            return;
+        }
 
+        uncompressedBinary = URICompressor.BinaryToU8(uncompressedBinary);
+        var cmprs = pako.deflate(uncompressedBinary, { level: 9});
+        return URICompressor.U8toBinary(cmprs);
     }
 
-    /**
-     * The function decompresses a base64 encoded string.
-     * @param base64str - The base64str parameter is a string that represents data encoded in base64
-     * format.
-     * @return - Returns the raw string (UTF-32) representation of the comressed data
-     * @note - Preforms decodeURIcomponent-- result string must be assumed to be UTF-32
-     */
-    static decompress(base64str)
+    static decompress(compressedBinary)
     {
+        if(!URICompressor.isBinaryString(compressedBinary))
+        {
+            console.warn("could not decompress string, not binary");
+            return;
+        }
 
+        var u8 = URICompressor.BinaryToU8(compressedBinary);
+        var dcmprs = pako.inflate(u8, { level: 9});
+        return URICompressor.U8toBinary(dcmprs);
+    }  
+
+    static U8toBinary(Uint8Array)
+    {
+        var result = "";
+        for(var ui8 of Uint8Array)
+        {
+            var byte = ui8.toString(2);
+            byte = byte.padStart(8, "0");
+            result += byte;
+        }
+        return result;
+    }
+
+    static BinaryToU8(BinaryStr)
+    {
+        var data = URICompressor.splitIntoChunks(BinaryStr, 8);
+        var holder = new Array();
+        for(var byte of data)
+        {
+            holder.push(Number.parseInt(byte, 2));
+        }
+        return new Uint8Array(holder);
+    }
+
+    static splitIntoChunks(str, size) 
+    {
+        var chunks = [];
+        for (var i = 0; i < str.length; i += size) {
+            chunks.push(str.slice(i, i + size));
+        }
+        return chunks;
+    }
+
+    static isBinaryString(str)
+    {
+        var isString = (typeof(str) == "string");
+        var isMod8 = (0 == (str.length % 8));
+        var isBinary = binaryOnly(str);
+        return isString && isMod8 && isBinary;
+
+        function binaryOnly(str)
+        {
+            var result = str.match(/[^01]/g); //does the string contain anything other than 1s and 0s?
+            if(result == null)
+            {
+                result = "";
+            }
+            var mod8 = (0 == (str.length % 8)); //is the string%8==0 (Uint8array data)
+            return (result.length == 0 && mod8);
+        }
     }
 }
